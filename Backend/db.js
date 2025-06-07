11f86c8e0440a41f85d1e5a0a12fffb6f0f7727a
kukil phukan <123075054+kphkns@users.noreply.github.com>
2025-06-08 00:35:33 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,12 @@
-// db.js
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'mentordb',
+  host: process.env.MYSQLHOST || 'localhost',
+  port: process.env.MYSQLPORT || 3306,
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'mentordb',
 });
 
 db.connect(err => {
